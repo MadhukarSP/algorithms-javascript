12b33bfb32af18bfdad5dce0b4cfcec1077b4308
Madhukar <madhutime@gmail.com>
2019-09-01 17:48:34 +0200

    Update BinarySearchTree.js

--- search/BinarySearchTree.js
+++ search/BinarySearchTree.js
@@ -10,6 +10,62 @@ class BST {
   constructor() {
     this.root = null;
   }
+  
+  add(data) {
+    let node = this.root;
+    if(node == null){
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function(data) {
+        if(data < node.data) {
+          if(node.left == null) {
+            node.left = new Node(data);
+            return;
+          } else {
+            return searchTree(node.left, data);
+          }
+        } else if (data > node.data) {
+          if(node.right == null) {
+            node.right = new Node(data);
+            return;
+          } else {
+            return searchTree(node.right, data);
+          }
+        } else {
+          return null;
+        }
+      }
+      return searchTree(data);
+    }
+  }
+    
+  findMin() {
+    let current = this.root;
+    while(current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+  
+  findMax() {
+    let current = this.root;
+    while(current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
 }
 
-console.log("Hello");
+let bst = new BST();
+bst.add(50);
+bst.add(30);
+bst.add(40);
+bst.add(20);
+bst.add(60);
+bst.add(70);
+bst.add(45);
+
+bst.findMax();
+bst.findMin();
+
